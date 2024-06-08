b1d7c9322b395d9b5045f2ab39f725f67fca5840
leomelegari <leonardo@leonardos-iMac-Pro.local>
2024-06-08 09:36:58 -0300

    create account methods created

---
+++ apps/web/src/app/auth/sign-up/actions.ts
@@ -0,0 +1,59 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
+import { signUpHttp } from '@/http/sign-up'
+
+const signUpSchema = z
+  .object({
+    name: z.string().refine((value) => value.split(' ').length > 1, {
+      message: 'Please, enter your full name',
+    }),
+    email: z
+      .string()
+      .email({ message: 'Please, provide a valid e-mail address' }),
+    password: z
+      .string()
+      .min(6, { message: 'Password should have at least 6 characters' }),
+    password_confirmation: z.string(),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: 'Password confirmation does not match.',
+    path: ['password_confirmation'],
+  })
+
+export async function signUpAction(data: FormData) {
+  // usei safeParse para lidar com o erro posteriormente. Se usassemos o parse apenas e houvessem erros, ele seria disparado imediatamente
+  const result = signUpSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+    return { success: false, message: null, errors }
+  }
+
+  const { name, email, password } = result.data
+
+  try {
+    await signUpHttp({
+      name,
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(error)
+    return {
+      success: false,
+      message: 'Unexpected error, try again',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
+}
--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,5 +1,25 @@
+import cookiesNext from 'cookies-next'
+import { CookiesFn } from 'cookies-next/lib/types'
 import ky from 'ky'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookieStore: CookiesFn | undefined
+
+        if (typeof window === 'undefined') {
+          const { cookies: serverCookies } = await import('next/headers')
+          cookieStore = serverCookies
+        }
+
+        const token = cookiesNext.getCookie('token', { cookies: cookieStore })
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
